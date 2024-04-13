9f412d2eb7935253f3d5640dd6827e41f7c55ee0
juandigilio <juanignaciodigilio@gmail.com>
2024-04-13 12:03:08 -0300

    Added TestEjercicios

---
+++ Assets/Scripts/Vec3Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MathDebbuger;
+
+using EjerciciosAlgebra;
+
+public class Vec3Test : MonoBehaviour
+{
+
+    private void Start()
+    {
+        //MathDebbuger.Vector3Debugger.AddVector(Vector3.zero, "Zero");
+        //MathDebbuger.Vector3Debugger.AddVector(Vector3.one * 5, "One");
+
+        
+    }
+}
