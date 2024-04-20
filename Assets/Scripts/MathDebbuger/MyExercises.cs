b4dec0309156510121e299a293b7767f3ba33bf1
juandigilio <juanignaciodigilio@gmail.com>
2024-04-20 12:04:58 -0300

    Added exercises 4, 5 & 6

--- Assets/Scripts/MathDebbuger/MyExercises.cs
+++ Assets/Scripts/MathDebbuger/MyExercises.cs
@@ -8,31 +8,57 @@ public class MyExercises : MonoBehaviour
     [SerializeField] Vector3 b1;
     [SerializeField] Vector3 c1;
 
+    [SerializeField] float duration = 1.0f;
+    private float elapsedTime = 0.0f;
+
 
     public Vector3 Exercise1()
     {
-        return c1 = a1 + b1;
+        return a1 + b1;
     }
 
     public Vector3 Exercise2()
     {
-        return c1 = b1 - a1;
+        return b1 - a1;
     }
 
     //x*x y*y z*z
     public Vector3 Exercise3()
     {
-        c1 = a1;
+        Vector3 newVec = a1;
 
-        c1.Scale(b1);
+        newVec.Scale(b1);
 
-        return c1;
+        return newVec;
     }
 
-    //esta mal!!!
+    //producto cruz
     public Vector3 Exercise4()
     {
-        return c1 = Vector3.Cross(a1, b1);
+        return Vector3.Cross(b1, a1);
+    }
+
+    public Vector3 Exercise5()
+    {
+        elapsedTime += Time.deltaTime;
+
+        float t = Mathf.Clamp01(elapsedTime / duration);
+
+        Vector3 newVec = Vector3.Lerp(a1, b1, t);
+
+        transform.position = newVec;
+
+        if (t >= 1.0f)
+        {
+            elapsedTime = 0.0f;
+        }
+
+        return newVec;
+    }
+
+    public Vector3 Exercise6()
+    {
+        return Vector3.Max(a1, b1);
     }
 
     void OnDrawGizmos()
@@ -43,7 +69,7 @@ public class MyExercises : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawLine(transform.position, transform.position + b1);
 
-        c1 = Exercise4();
+        c1 = Exercise6();
 
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(transform.position, transform.position + c1);
