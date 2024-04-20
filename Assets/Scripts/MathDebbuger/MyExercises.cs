9032554d11a34af4e6d84994b27bb3052ec9ca67
juandigilio <juanignaciodigilio@gmail.com>
2024-04-20 12:49:16 -0300

    Added MyExercises to gameplay

--- Assets/Scripts/MathDebbuger/MyExercises.cs
+++ Assets/Scripts/MathDebbuger/MyExercises.cs
@@ -1,16 +1,49 @@
+using MathDebbuger;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MyExercises : MonoBehaviour
 {
-    [SerializeField] Vector3 a1;
-    [SerializeField] Vector3 b1;
-    [SerializeField] Vector3 c1;
+    [SerializeField] private Vector3 a1;
+    [SerializeField] private Vector3 b1;
+    [SerializeField] private Vector3 c1;
 
-    [SerializeField] float duration = 1.0f;
+    [SerializeField] private float duration = 1.0f;
     private float elapsedTime = 0.0f;
 
+    private List<string> vectorId = new List<string>();
+
+    private void Start()
+    {
+        vectorId.Add("Vector 1");
+        vectorId.Add("Vector 2");
+        vectorId.Add("Vector 3");
+
+        Vector3Debugger.AddVector(a1, Color.white, vectorId[0]);
+        Vector3Debugger.AddVector(b1, Color.black, vectorId[1]);
+        Vector3Debugger.AddVector(c1, Color.red, vectorId[2]);
+    }
+
+    private void Update()
+    {
+        c1 = Exercise6();
+        Vector3Debugger.UpdatePosition(vectorId[0], a1);
+        Vector3Debugger.UpdatePosition(vectorId[1], b1);
+        Vector3Debugger.UpdatePosition(vectorId[2], c1);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + a1);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + b1);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + c1);
+    }
 
     public Vector3 Exercise1()
     {
@@ -59,19 +92,5 @@ public class MyExercises : MonoBehaviour
     public Vector3 Exercise6()
     {
         return Vector3.Max(a1, b1);
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + a1);
-
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, transform.position + b1);
-
-        c1 = Exercise6();
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, transform.position + c1);
     }
 }
