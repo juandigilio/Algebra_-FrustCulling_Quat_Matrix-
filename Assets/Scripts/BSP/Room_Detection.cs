2f6e993fa1f78e8e2154f09024b0ee88772888d7
juandigilio <juanignaciodigilio@gmail.com>
2024-05-14 16:06:25 -0300

    Updated RoomDetection
    Rays shown in frutums and detecting walls

--- Assets/Scripts/BSP/Room_Detection.cs
+++ Assets/Scripts/BSP/Room_Detection.cs
@@ -1,48 +1,135 @@
 using UnityEngine;
+using System.Collections.Generic;
+//using CustomMath;
 
 [System.Serializable]
 public class Room_Detection : MonoBehaviour
 {
-    [SerializeField] public Room room;
     [SerializeField] public Camera mainCamera;
+    [SerializeField] public Frustum frustum = new Frustum();
+    [SerializeField] public ObjectCulling objectCulling;
+    [SerializeField] public float initalAngle;
+    [SerializeField] public float cameraFov;
+    [SerializeField] public float otherFov;
 
+    public List<Transform> planes;
+
+    private List<Room> rooms = new List<Room>();
+
+    List<Vector3> nearPoint = new List<Vector3>();
+    List<Vector3> farPoint = new List<Vector3>();
+    private Vector3 nearLeftPoint;
+    private Vector3 farLeftPoint;
+    private Vector3 nearRightPoint;
+    private Vector3 farRightPoint;
 
     private void Awake()
     {
-        room = GetComponent<Room>();
-        
+        mainCamera = GetComponent<Camera>();
+        frustum = GetComponent<Frustum>();
+        objectCulling = GetComponent<ObjectCulling>();
     }
 
+    private void Start()
+    {
+        rooms = objectCulling.GetRooms();
+    }
     private void Update()
     {
         CheckCameraPosition();
     }
 
-    private void CheckCameraPosition()
+    private void OnDrawGizmos()
     {
-        if (room != null && mainCamera != null)
+        DrawRays();
+    }
+
+    private void GetRaysPoints(int totalLines)
+    {
+        List<Vector3> newNear = new List<Vector3>();
+        List<Vector3> newFar = new List<Vector3>();
+
+        nearLeftPoint = Vector3.Lerp(frustum.nearUpLeftV, frustum.nearDownLeftV, 0.5f);
+        farLeftPoint = Vector3.Lerp(frustum.farUpLeftV, frustum.farDownLeftV, 0.5f);
+
+        nearRightPoint = Vector3.Lerp(frustum.nearUpRightV, frustum.nearDownRightV, 0.5f);
+        farRightPoint = Vector3.Lerp(frustum.farUpRightV, frustum.farDownRightV, 0.5f);
+
+        newNear.Add(nearLeftPoint);
+        newFar.Add(farLeftPoint);
+
+        for (int i = 0; i < totalLines; i++)
         {
-            room.isVisible = true;
+            float t = (1.0f / totalLines) * ((float)i + 1.0f);
 
-            foreach (Transform normal in room.normals)
+            newNear.Add(Vector3.Lerp(nearLeftPoint, nearRightPoint, t));
+            newFar.Add(Vector3.Lerp(farLeftPoint, farRightPoint, t));
+        }
+
+        nearPoint = newNear;
+        farPoint = newFar;
+    }
+
+    private void DrawRays()
+    {
+        int totalLines = 16;
+
+        GetRaysPoints(totalLines);
+
+        for (int i = 0; i < totalLines; i++)
+        {
+            Vector3 lineStart = nearPoint[i];
+            Vector3 lineEnd = farPoint[i];
+
+            MyRaycastHit hit = new MyRaycastHit();
+
+            if (MyRaycast.CastRay(lineStart, (lineEnd - lineStart).normalized, out hit, planes))
             {
-                Plane plane = new Plane(normal.forward, normal.position);
+                Vector3 newFar = hit.point;
 
-                if (!plane.GetSide(mainCamera.transform.position))
+                float actualMagnitude = Vector3.Distance(nearPoint[i], farPoint[i]);
+                float newMagnitude = Vector3.Distance(nearPoint[i], newFar);
+
+                if (actualMagnitude >= newMagnitude)
                 {
-                    room.isVisible = false;
-                    break;
+                    lineEnd = hit.point;
+                    Debug.Log("La linea colisiona.");
                 }
             }
 
-            if (room.isVisible)
-            {
-                Debug.Log("La cámara está dentro de la habitación." + room);
-            }
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(lineStart, lineEnd);
         }
-        else
+    }
+
+    private void CheckCameraPosition()
+    {
+        foreach (Room room in rooms)
         {
-            Debug.LogWarning("Falta asignar la habitación o la cámara." + room);
+            if (room != null && mainCamera != null)
+            {
+                room.isVisible = true;
+
+                foreach (Transform normal in room.normals)
+                {
+                    Plane plane = new Plane(normal.forward, normal.position);
+
+                    if (!plane.GetSide(mainCamera.transform.position))
+                    {
+                        room.isVisible = false;
+                        break;
+                    }
+                }
+
+                if (room.isVisible)
+                {
+                    Debug.Log("La cámara está dentro de la habitación." + room);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Falta asignar la habitación o la cámara." + room);
+            }
         }
     }
 }
